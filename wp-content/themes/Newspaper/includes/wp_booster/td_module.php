043aa00a8b26c4d9b96e5cf0d42d40452e454955
JerryDing2014 <jerryd221@gmail.com>
2016-01-12 23:07:23 +1100

    update the new version

--- wp-content/themes/Newspaper/includes/wp_booster/td_module.php
+++ wp-content/themes/Newspaper/includes/wp_booster/td_module.php
@@ -159,9 +159,9 @@ abstract class td_module {
         $buffy = '';
         if (td_util::get_option('tds_p_show_comments') != 'hide') {
             $buffy .= '<div class="td-post-comments"><i class="td-icon-comments"></i>';
-            $buffy .= '<a href="' . get_comments_link($this->post->ID) . '">';
-            $buffy .= get_comments_number($this->post->ID);
-            $buffy .= '</a>';
+                $buffy .= '<a href="' . get_comments_link($this->post->ID) . '">';
+                    $buffy .= get_comments_number($this->post->ID);
+                $buffy .= '</a>';
             $buffy .= '</div>';
         }
 
